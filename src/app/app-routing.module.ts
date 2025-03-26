3fe8348d706ee78b2df867349e9a03c6e647eedb
Abraham Crispin <abraham.crispin@proveedoresgnp.mx>
2025-03-26 15:20:43 -0600

    mejora para proteger la rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,26 @@ import { AuthGuard } from './utils/guards/auth.guard';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {
-    path: 'welcome',
-    loadChildren: () => import('./components/welcome-page/welcome-page.module').then(m => m.WelcomePageModule),
-    // habilitar para usar Guard=> canActivate: [AuthGuard]
-  },
-  {
-    path: 'conversiones',
-    loadChildren: () => import('./components/conversiones/conversiones.module').then(m => m.ConversionesModule)
-  },
-  {
-    path: 'calcula-fecha',
-    loadChildren: () => import('./components/calcula-fecha/calcula-fecha.module').then(m => m.CalculaFechaModule)
-  },
-  {
-    path: 'formulario',
-    loadChildren: () => import('./components/formulario/formulario.module').then(m => m.FormularioModule)
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'welcome',
+        loadChildren: () => import('./components/welcome-page/welcome-page.module').then(m => m.WelcomePageModule),
+      },
+      {
+        path: 'conversiones',
+        loadChildren: () => import('./components/conversiones/conversiones.module').then(m => m.ConversionesModule),
+      },
+      {
+        path: 'calcula-fecha',
+        loadChildren: () => import('./components/calcula-fecha/calcula-fecha.module').then(m => m.CalculaFechaModule),
+      },
+      {
+        path: 'formulario',
+        loadChildren: () => import('./components/formulario/formulario.module').then(m => m.FormularioModule),
+      }
+    ]
   }
 ];
 
